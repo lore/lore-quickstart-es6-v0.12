82c17e8a4e6391269479c74a41493687d36407e9
Jason Hansen <jason_hansen@outlook.com>
2017-05-09 18:48:49 -0700

    [dialogs] step 3, add dialogs hook

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ lore.summon({
     connections: require('lore-hook-connections'),
     connect: require('lore-hook-connect'),
     dialog: require('lore-hook-dialog'),
+    dialogs: require('lore-hook-dialogs-bootstrap'),
     models: require('lore-hook-models'),
     react: require('lore-hook-react'),
     reducers: require('lore-hook-reducers'),
